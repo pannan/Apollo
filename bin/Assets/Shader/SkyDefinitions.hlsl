e8d08670eeaf71d866f4acc2ee769c12bf7937c6
pannan <xiongjg2706@gmail.com>
2018-09-04 20:25:23 +0800

    1继续sky shader

--- bin/Assets/Shader/SkyDefinitions.hlsl
+++ bin/Assets/Shader/SkyDefinitions.hlsl
@@ -2,7 +2,6 @@
 定义6个基本物理单位
 米(m)，纳米(nm),弧度(rad)，球面度(sr)，瓦(watt)，流明(lm)
 */
-
 const Length m = 1.0;
 const Wavelength nm = 1.0;
 const Angle rad = 1.0;
@@ -57,6 +56,37 @@ const Luminance kcd_per_square_meter = kcd / m2;
 #define Power float
 #define LuminousPower float
 
+/*
+大气层参数
+
+使用上面的类型，我们可以定义大气层模型的参数。
+我们由定义density profiles开始，它的参数依赖海拔：
+*/
+
+// An atmosphere layer of width 'width', and whose density is defined as
+//   'exp_term' * exp('exp_scale' * h) + 'linear_term' * h + 'constant_term',
+// clamped to [0,1], and where h is the altitude.
+struct DensityProfileLayer 
+{
+	Length width;
+	Number exp_term;
+	InverseLength exp_scale;
+	InverseLength linear_term;
+	Number constant_term;
+};
+
+// An atmosphere density profile made of several layers on top of each other
+// (from bottom to top). The width of the last layer is ignored, i.e. it always
+// extend to the top atmosphere boundary. The profile values vary between 0
+// (null density) to 1 (maximum density).
+/*
+一个大气层desity profile由几个层组成（从下到上）。最有一层的width被忽略，因为它总是延生到大气层的顶部边界。
+*/
+struct DensityProfile
+{
+	DensityProfileLayer layers[2];
+};
+
 struct AtmosphereParameters 
 {
 	// The solar irradiance at the top of the atmosphere.
@@ -64,45 +94,68 @@ struct AtmosphereParameters
 	IrradianceSpectrum solar_irradiance;
 	// The sun's angular radius. Warning: the implementation uses approximations
 	// that are valid only if this angle is smaller than 0.1 radians.
+	//太阳角度半径。Warming:这个执行近似的，只有当角度小于0.1弧度时有效。
 	Angle sun_angular_radius;
-	// The distance between the planet center and the bottom of the atmosphere.
+	//地心到大气层底部边界的距离
 	Length bottom_radius;
-	// The distance between the planet center and the top of the atmosphere.
+	//地心到大气层顶部边界的距离
 	Length top_radius;
-	// The density profile of air molecules, i.e. a function from altitude to
-	// dimensionless values between 0 (null density) and 1 (maximum density).
+	//空气分子的density profile
+	//从0（零密度）和1（最大密度）的关于高度的无量纲值函数。
 	DensityProfile rayleigh_density;
 	// The scattering coefficient of air molecules at the altitude where their
 	// density is maximum (usually the bottom of the atmosphere), as a function of
 	// wavelength. The scattering coefficient at altitude h is equal to
 	// 'rayleigh_scattering' times 'rayleigh_density' at this altitude.
+	/*
+	在空子分子密度最大的地方的散射系数（一般在大气层的底部），是一个关于波长的函数。
+	在海拔h的散射系数 = rayleigh_scattering * rayleigh_density
+	*/
 	ScatteringSpectrum rayleigh_scattering;
 	// The density profile of aerosols, i.e. a function from altitude to
 	// dimensionless values between 0 (null density) and 1 (maximum density).
+	//气溶胶的density profile
+	//从0（零密度）和1（最大密度）的关于高度的无量纲值函数。
 	DensityProfile mie_density;
 	// The scattering coefficient of aerosols at the altitude where their density
 	// is maximum (usually the bottom of the atmosphere), as a function of
 	// wavelength. The scattering coefficient at altitude h is equal to
 	// 'mie_scattering' times 'mie_density' at this altitude.
+	/*
+	在气溶胶密度最大的地方的散射系数（一般在大气层的底部），是一个关于波长的函数。
+	在海拔h的散射系数 = mie_scattering * mie_density
+	*/
 	ScatteringSpectrum mie_scattering;
 	// The extinction coefficient of aerosols at the altitude where their density
 	// is maximum (usually the bottom of the atmosphere), as a function of
 	// wavelength. The extinction coefficient at altitude h is equal to
 	// 'mie_extinction' times 'mie_density' at this altitude.
+	/*
+	在气溶胶密度最大的地方的消光系数（一般在大气层的底部），是一个关于波长的函数。
+	在海拔h的消光系数 = mie_extinction * mie_density
+	*/
 	ScatteringSpectrum mie_extinction;
 	// The asymetry parameter for the Cornette-Shanks phase function for the
 	// aerosols.
+	//关于气溶胶Cornette-Shanks相位函数的不对称参数
 	Number mie_phase_function_g;
 	// The density profile of air molecules that absorb light (e.g. ozone), i.e.
 	// a function from altitude to dimensionless values between 0 (null density)
 	// and 1 (maximum density).
+	//空气分子（比如臭氧）吸收光的density profile
+	//从0（零密度）和1（最大密度）的关于高度的无量纲值函数
 	DensityProfile absorption_density;
 	// The extinction coefficient of molecules that absorb light (e.g. ozone) at
 	// the altitude where their density is maximum, as a function of wavelength.
 	// The extinction coefficient at altitude h is equal to
 	// 'absorption_extinction' times 'absorption_density' at this altitude.
+	/*
+	在空气分子密度最大的地方的消光系数（比如臭氧），是一个关于波长的函数。
+	在海拔h的消光系数 = absorption_extinction * absorption_density
+	*/
 	ScatteringSpectrum absorption_extinction;
 	// The average albedo of the ground.
+	//地表的平均albedo
 	DimensionlessSpectrum ground_albedo;
 	// The cosine of the maximum Sun zenith angle for which atmospheric scattering
 	// must be precomputed (for maximum precision, use the smallest Sun zenith
--- bin/Assets/Shader/SkyFuntion.hlsl
+++ bin/Assets/Shader/SkyFuntion.hlsl
@@ -109,3 +109,199 @@ bool RayIntersectGround(in AtmosphereParameters atmopshere, Length r, Number mu)
 	return mu < 0.0 && atmopshere.bottom_radius * atmopshere.bottom_radius + r*r * (mu*mu - 1.0) >= 0.0;
 }
 
+
+/*
+Beer-Lambert law
+T = exp(-r)
+r : optical depth（光学深度）
+我们现在可以计算p和i之间的透视率。根据Beer-Lambert law，会涉及沿着线段[p,i]上的空气分子的密度的数值积分，以及气溶胶密度的数值积分和
+吸收光（比如臭氧）的空气分子的密度的数值积分。当线段[p,i]不和地面相交时，这个3个积分有一样的形式（exp(-r)），它们可以再接下来的一些辅助
+函数（使用trapezoidal rule）的帮助下进行数值计算。
+*/
+Number GetLayerDensity(in DesityProfileLayer layer, Length altitude)
+{
+	Number density = layer.exp_term * exp(layer.exp_scale * altitude) + layer.linear_term * altitude + layer.constant_term;
+	return clamp(density, Number(0.0), Number(1.0));
+}
+
+Number GetProfileDensity(in DensityProfile profile, Length altitude)
+{
+	return altitude < profile.layers[0].width ?
+		GetLayerDensity(profile.layers[0], altitude) :
+		GetLayerDensity(profile.layers[1], altitude);
+}
+
+Length ComputeOpticalLengthToTopAtmosphereBoundary(in AtmosphereParameters atmosphere, in DensityProfile profile, Length r, Number mu)
+{
+	//数值积分的间隔数量
+	const int SAMPLE_COUNT = 500;
+	//积分步长，每个积分间隔的长度
+	Length dx = DistanceToTopAtmosphereBoundary(atmosphere, r, mu) / Number(SAMPLE_COUNT);
+	//积分循环
+	Length result = 0.0;
+	for (int i = 0; i <= SAMPLE_COUNT; ++i)
+	{
+		Length d_i = dx * i;
+		//当前采样点到地心的距离
+		//根据||pi|| * ||pi|| + 2 * r * mu * ||pi|| + r*r = R * R ->R = sqrt(d*d + 2*r*d*mu + r*r)
+		Length r_i = sqrt(d_i*d_i + 2 * d_i*mu*r + r*r);
+		//当前采样点的密度（用大气层底部的密度来除得到一个无量纲的数(指没有单位的物流量)）
+		Number y_i = GetProfileDensity(profile, r_i - atmosphere.bottom_radius);
+		//采样权重
+		Number weight_i = i == 0 || i == SAMPLE_COUNT ? 0.5 : 1.0;
+		result += y_i * weight * dx;
+	}
+
+	return result;
+}
+
+/*
+p和i之间的透视率现在很容易计算了（我们继续假设线段没有和地面相交）
+*/
+DimensionlessSpectrum ComputeTransmittanceToTopAtmosphereBoundary(in AtmosphereParameters atmosphere, Length r, Number mu)
+{
+	Number opticalLength_rayleigh = ComputeOpticalLengthToTopAtmosphereBoundary(atmosphere, atmosphere.rayleigh_density, r, mu);
+	Number opticalLength_mie = ComputeOpticalLengthToTopAtmosphereBoundary(atmosphere, atmosphere.mie_density, r, mu);
+	Number opticalLength_absorption = ComputeOpticalLengthToTopAtmosphereBoundary(atmosphere, atmosphere.absorption_density, r, mu);
+	
+	opticalLength_rayleigh *= atmosphere.rayleigh_scattering;
+	opticalLength_mie *= atmosphere.mie_extinction;
+	opticalLength_absorption *= atmosphere.absorption_extinction;
+
+	Number opticalLength = opticalLength_rayleigh + opticalLength_mie + opticalLength_absorption;
+
+	return exp(-opticalLength);
+}
+
+/*
+预计算
+extrapolated：推断
+{
+在数学中，外推是在原始观察范围之外，根据与另一个变量的关系估计变量的值的过程。 
+它类似于插值，它在已知观测值之间产生估计值，但是外推会受到更大的不确定性和产生无意义结果的更高风险。
+}
+
+上述函数的评估成本很高，计算单次和多次散射需要很多的评估。
+幸运的是函数只依赖两个参数并且什么平滑（应该是值函数没有高频形状）
+我们可以在一个2d纹理里预计算来优化评估
+
+这样我们需要在函数参数(r,mu)和纹理坐标(u,v)之间做一个映射，反之也然，
+因为这些参数不具有一致的单位和值范围。
+即使是这种情况，在大小为n的纹理中以间隔[0,1]储存函数f将在0.5/n，1.5/n，......(n - 0.5)/n采样函数
+因为纹理采样时在texels的中心（所以从0.5开始，以1为间隔）。
+因此，在纹理的边界（0和1）只能给出我们推断的函数值（extrapolated function values）。
+【上面的意思我理解是：因为前面的采样时从0.5开始，(n-0.5)/n结束，所以在纹理的边界
+0和1是没有采样的，所以只能通过推断（插值）来获得值】
+
+为了避免这种情况，我们需要在纹理元素0的中心储存f(0)（而不是f(0.5)）
+在纹理元素n-1的中心储存f(1)（而不是f((n-0.5)/n)）。
+可以通过以下的映射来完成:
+从值x[0,1]到纹理坐标u[0.5/n,(n - 0.5)/n] 和它的inverse
+*/
+
+Number GetTextureCoordFromUnitRange(Number x, int texture_size)
+{
+	//(0.5 - x) / Number(texture_size) + x
+	return 0.5 / Number(texture_size) + x * (1.0 - 1.0 / Number(texture_size));
+}
+
+Number GetUnitRangeFromTextureCoord(Number u, int texture_size)
+{
+	return (u - 0.5 / Number(texture_size)) / (1.0 - 1.0 / Number(texture_size));
+}
+
+/*
+利用这些函数，我们现在可以定义一个(r,mu)和纹理坐标的映射和它的inverse，并避免在纹理查询时的任何extrapolation。
+在最初的实现中，这种映射使用了为地球大气状况选择的一些特殊常数。
+在这里我们使用一般映射，适用于任何的大气层，但是任然在地平线附近提供更高的采样率。
+我们的改进是基于在我们的论文中4D纹理的参数化描述：
+对r我们使用一样的映射
+对mu的映射有稍微的改进（只考虑view ray不和地面相交的情况）
+更进一步，参考到大气层顶部边界的距离d，相对它的最大值和最小值 
+
+----------
+        -- t---------
+		    |              ---------
+dmin	|			            ------------
+			|					             --------------
+			|								       ---------------
+			P									           -------------- 
+            |													----------				
+ 	-------------------------								--------
+                              ----------------e---						 ------
+		       			                     ------------							 i-------
+					      			                      ----------						------ 大气层
+								
+										
+
+上面所示：
+点p代表相交
+view ray和大气层相交的最短距离是 ||pt||
+view ray和大气层相交的最长距离是||pe|| + ||ei||
+||pe|| = p
+||ei|| = H
+点e是view ray和地面相切的点
+i是veiw ray和大气层相交的点
+
+dmin = rtop - r
+dmax = k + H
+我们映射mu到一个范围为[0,1]的值Xmu
+
+有了这些定义，我们映射(r,mu)到纹理坐标(u,v)可以如下执行:
+*/
+
+float2 GetTransmittanceTextureUvFromRMu(in AtmosphereParameters atmosphere, Length r, Number mu)
+{
+	//O为地心
+	//为了求view ray和大气层距离d的最大值，需要求H和K
+	//求||ei||的距离H，因为e是和地球相交切线的交点，所以||Oe||垂直于view ray[p,i]
+	//这样(O,e,i)组成一个直角三角形，斜边为[O,i]
+	//H = ||ei|| = sqrt(||Oi||*||Oi|| - ||Oe||*||Oe||)
+	//而||Oi|| = atmosphere.top_radius
+	//||Oe|| = atmosphere.bottom_radius
+	Length H = sqrt(atmosphere.top_radius * atmosphere.top_radius - atmosphere.bottom_radius * atmosphere.bottom_radius);
+	
+	//同理(O,p,e)组成一个直角三角形，斜边为[O,p]
+	//k = ||Oe|| = sqrt(||Op||*||Op|| - ||Oe||*||Oe||)
+	//而||Op|| = r
+	//||Oe|| = atmosphere.bottom_radius
+	Length K = sqrt(r * r - atmosphere.bottom_radius * atmosphere.bottom_radius);
+
+	Length d = DistanceToTopAtmosphereBoundary(atmosphere, r, mu);
+	Length d_min = atmosphere.top_radius - r;
+	Length d_max = H + K;
+	Number x_mu = (d - d_min) / (d_max - d_min);
+	Number x_r = K / H;
+
+	Number u = GetTextureCoordFromUnitRange(x_mu, TRANSMITTANCE_TEXTURE_WIDTH);
+	Number v = GetTextureCoordFromUnitRange(x_r, TRANSMITTANCE_TEXTURE_WIDTH);
+	return float2(u, v);
+}
+
+//上面函数的逆过程
+void GetRMuFromTransmittanceTextureUv(in AtmosphereParameters atmosphere, in float2 uv, out Length r, out Number mu)
+{
+	Number x_mu = GetUintRangeFromTextureCoord(uv.x, TRANSMITTANCE_TEXTURE_WIDTH);
+	Number x_r = GetUintRangeFromTextureCoord(uv.y, TRANSMITTANCE_TEXTURE_WIDTH);
+
+	Length H = sqrt(atmosphere.top_radius * atmosphere.top_radius - atmosphere.bottom_radius * atmosphere.bottom_radius);
+	Length K = H * x_r;
+	r = sqrt(K * K + atmosphere.bottom_radius * atmosphere.bottom_radius);
+
+	Length d_min = atmosphere.top_radius - r;
+	Length d_max = H + K;
+	Length d = x_mu * (d_max - d_min) + d_min;
+	//从上面知道，d_min是mu = 1时出现，当d == 0时，mu = 1
+	mu = d == 0.0 ? Number(1.0) : (H * H - K * K - d * d) / (2.0 * r * d);
+	mu = ClampCosine(mu);
+}
+
+//现在可以用一个函数预计算透视率纹理
+DimensionlessSpectrum ComputeTransmittanceToTopAtmosphereBoundaryTexture(in AtmosphereParameters atmosphere, in float2 uv)
+{
+	const float2 TRANSMITTANCE_TEXTURE_SIZE = float2(TRANSMITTANCE_TEXTURE_WIDTH, TRANSMITTANCE_TEXTURE_HEIGHT);
+	Length r;
+	Number mu;
+	GetRMuFromTransmittanceTextureUv(atmosphere, gl_frag_coord / TRANSMITTANCE_TEXTURE_SIZE, r, mu);
+	return ComputeTransmittanceToTopAtmosphereBoundary(atmosphere, r, mu);
+}
